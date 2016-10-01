6c07b004abadf5c1502d667802a41d66d7a09d60
ErrorCraftLP <Nils.tePoel@gmx.de>
2016-10-01 18:08:15 +0200

    Revert version number to 1.0.0
    I'm preparing the public release on curse now :)

--- src/main/java/de/errorcraftlp/cryingobsidian/Utils.java
+++ src/main/java/de/errorcraftlp/cryingobsidian/Utils.java
@@ -11,7 +11,7 @@ public class Utils {
 	// Constants
 	public static final String ID = "cryingobsidian";
 	public static final String NAME = "Crying Obsidian Mod";
-	public static final String VERSION = "2.1.0";
+	public static final String VERSION = "1.0.0";
 	public static final String CLIENT_PROXY = "de.errorcraftlp.cryingobsidian.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "de.errorcraftlp.cryingobsidian.proxy.ServerProxy";
 
