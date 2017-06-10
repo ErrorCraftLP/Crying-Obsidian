8f1e65df9a43a7da93f86730b67c38a8f4c48795
ErrorCraftLP <Nils.tePoel@gmx.de>
2017-06-10 15:48:31 +0200

    Backport changes from 1.11 to 1.10

--- src/main/java/de/errorcraftlp/cryingobsidian/CryingObsidian.java
+++ src/main/java/de/errorcraftlp/cryingobsidian/CryingObsidian.java
@@ -7,6 +7,7 @@ import de.errorcraftlp.cryingobsidian.item.ItemCryingObsidian;
 import de.errorcraftlp.cryingobsidian.proxy.ServerProxy;
 import de.errorcraftlp.cryingobsidian.tileentiy.TileEntityCryingObsidianAdvanced;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
@@ -17,6 +18,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 @Mod(modid = Utils.ID, name = Utils.NAME, version = Utils.VERSION, updateJSON = Utils.UPDATE_JSON, acceptedMinecraftVersions = Utils.ACCEPTED_VERSIONS)
@@ -68,6 +70,11 @@ public class CryingObsidian {
 		// Register tile entities
 		GameRegistry.registerTileEntity(TileEntityCryingObsidianAdvanced.class, "crying_obsidian_advanced_tile_entity");
 
+		// Register crying obsidian blocks in ore dictionary
+		OreDictionary.registerOre("obsidian", cryingObsidianBlock);
+		OreDictionary.registerOre("obsidian", cryingObsidianBlockDecoration);
+		OreDictionary.registerOre("obsidian", cryingObsidianBlockAdvanced);
+
 	}
 
 	@EventHandler
@@ -84,7 +91,7 @@ public class CryingObsidian {
 	public void registerRecipes() {
 
 		GameRegistry.addRecipe(new ShapelessOreRecipe(cryingObsidianBlock,
-				"gemLapis", "gemLapis", "gemLapis", "gemLapis", "obsidian"
+				"gemLapis", "gemLapis", "gemLapis", "gemLapis", Blocks.OBSIDIAN
 				));
 
 		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(cryingObsidianBlockDecoration, 4),
