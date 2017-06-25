64b8ccce4dfa49fbd709c7b25435c152c3fa75fd
ErrorCraftLP <Nils.tePoel@gmx.de>
2017-06-25 11:06:28 +0200

    Update Forge/MCP, use new registry system

--- src/main/java/de/errorcraftlp/cryingobsidian/CryingObsidian.java
+++ src/main/java/de/errorcraftlp/cryingobsidian/CryingObsidian.java
@@ -9,14 +9,19 @@ import de.errorcraftlp.cryingobsidian.tileentiy.TileEntityCryingObsidianAdvanced
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.registries.IForgeRegistry;
 
+@EventBusSubscriber
 @Mod(modid = Utils.ID, name = Utils.NAME, version = Utils.VERSION, updateJSON = Utils.UPDATE_JSON, acceptedMinecraftVersions = Utils.ACCEPTED_VERSIONS)
 public class CryingObsidian {
 
@@ -24,33 +29,39 @@ public class CryingObsidian {
 	@SidedProxy(clientSide = Utils.CLIENT_PROXY, serverSide = Utils.SERVER_PROXY)
 	public static ServerProxy proxy;
 
-	// Block/Item-related variables
-	public static Block cryingObsidianBlock;
-	public static Block cryingObsidianBlockAdvanced;
-	public static Block cryingObsidianBlockDecoration;
-	public static Item cryingObsidianItem;
+	// Blocks/Items
+	public static Block cryingObsidianBlock = new BlockCryingObsidian();
+	public static Block cryingObsidianBlockAdvanced = new BlockCryingObsidianAdvanced();
+	public static Block cryingObsidianBlockDecoration = new BlockCryingObsidianDecoration();
+	public static Item cryingObsidianItem = new ItemCryingObsidian();
+
+	@SubscribeEvent
+	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
+
+		final IForgeRegistry<Block> registry = event.getRegistry();
+		registry.register(cryingObsidianBlock);
+		registry.register(cryingObsidianBlockAdvanced);
+		registry.register(cryingObsidianBlockDecoration);
+
+	}
+
+	@SubscribeEvent
+	public static void registerItems(final RegistryEvent.Register<Item> event) {
+
+		final IForgeRegistry<Item> registry = event.getRegistry();
+
+		// Item Blocks
+		registry.register(new ItemBlock(cryingObsidianBlock).setRegistryName("crying_obsidian_block"));
+		registry.register(new ItemBlock(cryingObsidianBlockAdvanced).setRegistryName("crying_obsidian_block_advanced"));
+		registry.register(new ItemBlock(cryingObsidianBlockDecoration).setRegistryName("crying_obsidian_block_decoration"));
+
+		// Item
+		registry.register(cryingObsidianItem);
+
+	}
 
 	@EventHandler
 	public void preInit(@SuppressWarnings("unused") final FMLPreInitializationEvent event) {
-
-		// Register crying obsidian block
-		cryingObsidianBlock = new BlockCryingObsidian();
-		GameRegistry.register(cryingObsidianBlock);
-		GameRegistry.register(new ItemBlock(cryingObsidianBlock).setRegistryName("crying_obsidian_block"));
-
-		// Register crying obsidian block (decoration variant)
-		cryingObsidianBlockDecoration = new BlockCryingObsidianDecoration();
-		GameRegistry.register(cryingObsidianBlockDecoration);
-		GameRegistry.register(new ItemBlock(cryingObsidianBlockDecoration).setRegistryName("crying_obsidian_block_decoration"));
-
-		// Register crying obsidian block (advanced variant)
-		cryingObsidianBlockAdvanced = new BlockCryingObsidianAdvanced();
-		GameRegistry.register(cryingObsidianBlockAdvanced);
-		GameRegistry.register(new ItemBlock(cryingObsidianBlockAdvanced).setRegistryName("crying_obsidian_block_advanced"));
-
-		// Register crying obsidian item
-		cryingObsidianItem = new ItemCryingObsidian();
-		GameRegistry.register(cryingObsidianItem);
 
 		// Register tile entities
 		GameRegistry.registerTileEntity(TileEntityCryingObsidianAdvanced.class, "crying_obsidian_advanced_tile_entity");
