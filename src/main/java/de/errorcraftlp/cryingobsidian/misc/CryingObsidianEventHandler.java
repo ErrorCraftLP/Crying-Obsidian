6704e5d9b18695bc092dd72e11316d505a4c9cfe
ErrorCraftLP <ErrorCraftLP@users.noreply.github.com>
2020-07-06 14:37:28 +0200

    All error messages are now displayed with a red color

--- src/main/java/de/errorcraftlp/cryingobsidian/misc/CryingObsidianEventHandler.java
+++ src/main/java/de/errorcraftlp/cryingobsidian/misc/CryingObsidianEventHandler.java
@@ -2,6 +2,7 @@ package de.errorcraftlp.cryingobsidian.misc;
 
 import net.minecraft.entity.player.EntityPlayer.SleepResult;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -13,7 +14,9 @@ public class CryingObsidianEventHandler {
 		// Disable sleeping in beds if the config option is enabled
 		if(CryingObsidianConfig.disableBeds) {
 			event.setResult(SleepResult.OTHER_PROBLEM);
-			event.getEntityPlayer().sendMessage(new TextComponentTranslation("message.bed_disabled"));
+			final TextComponentTranslation message = new TextComponentTranslation("message.bed_disabled");
+			message.getStyle().setColor(TextFormatting.RED);
+			event.getEntityPlayer().sendMessage(message);
 		}
 	}
 }
